2302efabc78fc4cb335fa5f23b4ba1a7883784ae
Misu <michelleshrestha17@gmail.com>
2026-09-16 19:58:53 +0545

    Board created, and added some explaination in txt file

--- tictactoe.py
+++ tictactoe.py
@@ -1,4 +1,5 @@
 import numpy as np
+import csv, pandas as pd
 
 #Board
 class Board:
@@ -7,5 +8,16 @@ class Board:
         self.cols = cols
         self.size = self.rows * self.cols
         #self.board = np.arange(0, self.size).reshape(rows,cols).astype(object) or
-        self.board = np.full((self.rows, self.cols), "_")
+        self.board = np.full((self.rows, self.cols), " _ ")
 
+    def game_board(self):
+        for i in range (self.rows):
+            print(" -" * (self.cols*3 + 1))
+            for j in range (self.cols):
+                print(" | "+ self.board[i][j], end="")
+            print(" | ")
+        print(" -" * (self.cols*3 + 1))
+
+
+b = Board(3,3)
+(b.game_board())
